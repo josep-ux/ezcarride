df80e055a0163ff4bed752b4755f1c5fda665493
Emeka Joseph <emeka4ict@gmail.com>
2026-06-24 22:17:55 +0100

    changes in the validation1

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password','user_type','country','dob','phone_number'])]
+#[Fillable(['name', 'email', 'password','user_type','country','dob',
+'phone_number','address','city','state','zip_code','license_number','vehicle_type','vehicle_model','vehicle_color','vehicle_plate_number','bank_name','bank_account_number','bank_account_name','payment_method','stripe_account_id','paypal_email','profile_image','status','curr_lat','curr_long'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
