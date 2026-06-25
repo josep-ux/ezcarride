dbdd86031da6ff75dedadbf3e5ebb4ae3c41fb69
Emeka Joseph <emeka4ict@gmail.com>
2026-06-25 18:39:47 +0100

    version 1 update in user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -67,10 +67,10 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('driver_id')->nullable()->constrained('users');
             $table->string('status')->default('pending'); // pending, accepted, ongoing, completed
-            $table->decimal('pickup_latitude', 6, 3);
-            $table->decimal('pickup_longitude', 6, 3);
-            $table->decimal('dropoff_latitude', 6, 3);
-            $table->decimal('dropoff_longitude', 6, 3);
+            $table->decimal('pickup_latitude', 3, 6);
+            $table->decimal('pickup_longitude', 3, 6);
+            $table->decimal('dropoff_latitude', 3, 6);
+            $table->decimal('dropoff_longitude', 3, 6);
             $table->decimal('fare', 8, 2);
             $table->timestamps();
         });
@@ -89,7 +89,7 @@ return new class extends Migration
             // $table->point('dropoff_location');
             // $table->string('pickup_address');
             // $table->string('dropoff_address');
-            $table->decimal('pickup_lat');
+            $table->decimal('pickup_lat', 3, 6);
             $table->decimal('pickup_long');
             $table->decimal('dropoff_lat');
             $table->decimal('dropoff_long');
