4c9a4bd10220b26d9a26448726b89698f0b6646a
Emeka Joseph <emeka4ict@gmail.com>
2026-06-26 15:49:25 +0100

    the auth updated file

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'curr_lat' => 'decimal:8',
+            'curr_long' => 'decimal:8',
         ];
     }
     public function trips(){
