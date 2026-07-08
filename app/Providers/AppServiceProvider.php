1863d633a8f3249c6c57dcc877b9203fd7250eee
Emeka Joseph <emeka4ict@gmail.com>
2026-07-09 00:10:50 +0100

    version 1 updating email sending

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+        // Point this directly to your frontend password reset page
+        return 'https://ezcarride-ng-mainnng-ikqe9v.laravel.cloud>getEmailForPasswordReset()';
+    });
+
     }
 }
