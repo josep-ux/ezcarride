e941c14933de6581b2d7fe4d0dc6056ab8feda86
Emeka Joseph <emeka4ict@gmail.com>
2026-07-09 14:31:22 +0100

    feat: implement 6 digit mobile password reset pin

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,35 +36,37 @@ class User extends Authenticatable
             'is_available' => 'boolean',
         ];
     }
+    
     public function trips(){
         return $this->hasMany(Trip::class,'user_id');
     }
 
     public function sendPasswordResetNotification($token){
-    // Convert the long alphanumeric Laravel token into a secure 6-digit numeric PIN
-    $pinCode = crc32($token) % 1000000;
-    $pinCode = str_pad(abs($pinCode), 6, '0', STR_PAD_LEFT);
+        // Convert the long alphanumeric Laravel token into a secure 6-digit numeric PIN
+        $pinCode = crc32($token) % 1000000;
+        $pinCode = str_pad(abs($pinCode), 6, '0', STR_PAD_LEFT);
 
-    $this->notify(new class($pincode) extends \Illuminate\Notification\Notification {
-        protected $pin;
+        // FIXED: Changed $pincode to $pinCode, and \Notification to plural \Notifications
+        $this->notify(new class($pinCode) extends \Illuminate\Notifications\Notification {
+            protected $pin;
 
-        public function __construct($pin) {
-            $this->pin = $pin;
-        }
+            public function __construct($pin) {
+                $this->pin = $pin;
+            }
 
-        public function via($notifiable) {
-            return ['mail'];
-        }
+            public function via($notifiable) {
+                return ['mail'];
+            }
 
-        public function toMail($notifiable) {
-            return (new \Illuminate\Notifications\Messages\MailMessage)
-                ->subject('Your Password Reset Code')
-                ->line('You are receiving this email because we received a password reset request for your mobile account.')
-                ->heading('Your Reset Code:')
-                ->line($this->pin)
-                ->line('Enter this 6-digit code in your mobile app to securely reset your password.')
-                ->line('If you did not request this, please ignore this email.');
-        }
-    });
- }
+            public function toMail($notifiable) {
+                return (new \Illuminate\Notifications\Messages\MailMessage)
+                    ->subject('Your Password Reset Code')
+                    ->line('You are receiving this email because we received a password reset request for your mobile account.')
+                    ->heading('Your Reset Code:')
+                    ->line($this->pin)
+                    ->line('Enter this 6-digit code in your mobile app to securely reset your password.')
+                    ->line('If you did not request this, please ignore this email.');
+            }
+        });
+    }
 }
